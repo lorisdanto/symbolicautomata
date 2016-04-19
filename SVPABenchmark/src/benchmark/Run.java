796b3ce09e64d205c7d3ead8193d4d36ab56ebcb
lorisdanto <lorisdanto@gmail.com>
2016-04-19 16:03:41 -0500

    added automatark

---
+++ SVPABenchmark/src/benchmark/Run.java
@@ -0,0 +1,12 @@
+package benchmark;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+
+}
---
+++ SVPABenchmark/src/benchmark/ltlconverter/LTLConverter.java
@@ -0,0 +1,18 @@
+package benchmark.ltlconverter;
+
+import LTLparser.FormulaNode;
+import logic.ltl.LTLFormula;
+import theory.BooleanAlgebra;
+import theory.bdd.BDD;
+
+public class LTLConverter {
+
+	public static LTLFormula<BDD, BDD> getLTLBDD(BooleanAlgebra<BDD,BDD> ba, FormulaNode phi){
+		
+		
+		
+		return null;
+	}
+	
+	
+}
