76fd3e47d92a4cd18c3a6ea012166578ecf473e6
lorisdanto <lorisdanto@gmail.com>
2016-04-21 17:33:23 -0500

    some changes

--- SVPABenchmark/src/benchmark/Run.java
+++ SVPABenchmark/src/benchmark/Run.java
@@ -1,12 +1,90 @@
 package benchmark;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.sat4j.specs.TimeoutException;
+
+import LTLparser.LTLNode;
+import LTLparser.LTLParserProvider;
+import automata.safa.SAFA;
+import automata.safa.booleanexpression.SumOfProductsFactory;
+import benchmark.ltlconverter.LTLConverter;
+import logic.ltl.LTLFormula;
+import logic.ltl.True;
+import theory.bdd.BDD;
+import theory.bddalgebra.BDDSolver;
+import utilities.Pair;
+
 public class Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static long timeout = 30000;
 
-		
+	public static void main(String[] args) {
+		RunSelfEquivLTL();
 	}
 
+	public static void RunSelfEquivLTL() {
+
+		try {
+			Files.walk(Paths.get("../automatark/LTL/")).forEach(filePath -> {
+				if (Files.isRegularFile(filePath)) {
+					try {
+						RunSelfEquivLTLFile(filePath);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void RunSelfEquivLTLFile(Path filePath) throws IOException {
+		// parser p;
+		// p.
+		List<LTLNode> nodes = LTLParserProvider.parse(new FileReader(filePath.toFile()));
+
+		for (LTLNode ltl : nodes) {
+			Pair<BDDSolver,LTLFormula<BDD, BDD>> pair = LTLConverter.getLTLBDD(ltl);
+			BDDSolver bdds = pair.first;
+			LTLFormula<BDD, BDD> tot = pair.second.pushNegations(bdds);
+			SAFA<BDD, BDD> safa = tot.getSAFA(bdds);
+
+			long startTime = System.currentTimeMillis();
+
+			boolean b = true;
+			long stopTime = System.currentTimeMillis();
+
+			try {
+				b = SAFA.isEquivalent(safa, safa, bdds, SumOfProductsFactory.getInstance());
+				stopTime = System.currentTimeMillis();
+			} catch (TimeoutException toe) {
+				stopTime = System.currentTimeMillis() + timeout;
+			}
+
+			long elapsedTime = stopTime - startTime;
+			System.out.println(elapsedTime);
+
+			startTime = System.currentTimeMillis();
+
+			Pair<Boolean, List<BDD>> b1 = SAFA.areReverseEquivalent(safa, safa, bdds);
+
+			stopTime = System.currentTimeMillis();
+			elapsedTime = stopTime - startTime;
+			System.out.println(elapsedTime);
+
+			if (b != b1.first)
+				throw new IllegalArgumentException("b and b1 should be the same");
+		}
+	}
 
 }
--- SVPABenchmark/src/benchmark/ltlconverter/LTLConverter.java
+++ SVPABenchmark/src/benchmark/ltlconverter/LTLConverter.java
@@ -1,18 +1,197 @@
 package benchmark.ltlconverter;
 
+import java.util.HashMap;
+import java.util.Set;
+
+import LTLparser.AlwaysNode;
+import LTLparser.AndNode;
+import LTLparser.DualNode;
+import LTLparser.EquivalenceNode;
+import LTLparser.EventuallyNode;
+import LTLparser.FalseNode;
 import LTLparser.FormulaNode;
+import LTLparser.IdNode;
+import LTLparser.ImplicationNode;
+import LTLparser.NegationNode;
+import LTLparser.NextNode;
+import LTLparser.OrNode;
+import LTLparser.ReleaseNode;
+import LTLparser.StrongReleaseNode;
+import LTLparser.TrueNode;
+import LTLparser.UntilNode;
+import LTLparser.WeakUntilNode;
+import LTLparser.XorNode;
+import logic.ltl.And;
+import logic.ltl.Eventually;
+import logic.ltl.False;
+import logic.ltl.Globally;
 import logic.ltl.LTLFormula;
-import theory.BooleanAlgebra;
+import logic.ltl.Next;
+import logic.ltl.Not;
+import logic.ltl.Or;
+import logic.ltl.Predicate;
+import logic.ltl.True;
+import logic.ltl.Until;
+import logic.ltl.WeakUntil;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import theory.bdd.BDD;
+import theory.bddalgebra.BDDSolver;
+import utilities.Pair;
 
 public class LTLConverter {
 
-	public static LTLFormula<BDD, BDD> getLTLBDD(BooleanAlgebra<BDD,BDD> ba, FormulaNode phi){
-		
-		
-		
-		return null;
+	public static Pair<BDDSolver, LTLFormula<BDD, BDD>> getLTLBDD(FormulaNode phi) {
+		Set<String> atoms = phi.returnLeafNodes();
+		HashMap<String, Integer> atomToInt = new HashMap<String, Integer>();
+		for (String atom : atoms)
+			atomToInt.put(atom, atomToInt.size());
+		BDDSolver bdds = new BDDSolver(atomToInt.size());
+		return new Pair<BDDSolver, LTLFormula<BDD, BDD>>(bdds, getLTLBDD(phi, atomToInt, bdds));
 	}
-	
-	
+
+	@SuppressWarnings("restriction")
+	public static LTLFormula<BDD, BDD> getLTLBDD(FormulaNode phi, HashMap<String, Integer> atomToInt, BDDSolver bdds) {
+
+		if (phi instanceof AlwaysNode) {
+			AlwaysNode cphi = (AlwaysNode) phi;
+			LTLFormula<BDD, BDD> subphi = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+
+			return new Globally<BDD, BDD>(subphi);
+		} else {
+			if (phi instanceof AndNode) {
+				AndNode cphi = (AndNode) phi;
+				LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+				LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(), atomToInt, bdds);
+
+				return new And<BDD, BDD>(left, right);
+			} else {
+				if (phi instanceof DualNode) {
+					DualNode cphi = (DualNode) phi;
+					LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(),
+							atomToInt, bdds);
+					LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(),
+							atomToInt, bdds);
+
+					return new Not<BDD, BDD>(
+							new Until<>(new Not<>(left), new Next<>(right)));
+				} else {
+					if (phi instanceof EquivalenceNode) {
+						EquivalenceNode cphi = (EquivalenceNode) phi;
+						LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+						LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(), atomToInt, bdds);
+
+						return new And<BDD, BDD>(new Or<BDD, BDD>(new Not<BDD, BDD>(left), right),
+								new Or<BDD, BDD>(right, new Not<BDD, BDD>(left)));
+					} else {
+						if (phi instanceof EventuallyNode) {
+							EventuallyNode cphi = (EventuallyNode) phi;
+							LTLFormula<BDD, BDD> subphi = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+
+							return new Eventually<BDD, BDD>(subphi);
+						} else {
+							if (phi instanceof FalseNode) {
+								return new False<BDD, BDD>();
+							} else {
+								if (phi instanceof IdNode) {
+									IdNode cphi = (IdNode) phi;
+									return new Predicate<BDD, BDD>(bdds.factory.ithVar(atomToInt.get(cphi.getName())));
+								} else {
+									if (phi instanceof ImplicationNode) {
+										ImplicationNode cphi = (ImplicationNode) phi;
+										LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+										LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(), atomToInt, bdds);
+
+										return new Or<BDD, BDD>(new Not<>(left), right);
+									} else {
+										if (phi instanceof NegationNode) {
+											NegationNode cphi = (NegationNode) phi;
+											LTLFormula<BDD, BDD> subphi = getLTLBDD(cphi.getMyLTL1(), atomToInt, bdds);
+											return new Not<BDD, BDD>(subphi);
+										} else {
+											if (phi instanceof NextNode) {
+												NextNode cphi = (NextNode) phi;
+												LTLFormula<BDD, BDD> subphi = getLTLBDD(cphi.getMyLTL1(), atomToInt,
+														bdds);
+												return new Next<BDD, BDD>(subphi);
+											} else {
+												if (phi instanceof OrNode) {
+													OrNode cphi = (OrNode) phi;
+													LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(), atomToInt,
+															bdds);
+													LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(), atomToInt,
+															bdds);
+
+													return new Or<BDD, BDD>(left, right);
+												} else {
+													if (phi instanceof ReleaseNode) {
+														ReleaseNode cphi = (ReleaseNode) phi;
+														LTLFormula<BDD, BDD> left = getLTLBDD(cphi.getMyLTL1(),
+																atomToInt, bdds);
+														LTLFormula<BDD, BDD> right = getLTLBDD(cphi.getMyLTL2(),
+																atomToInt, bdds);
+
+														return new Not<BDD, BDD>(
+																new Until<>(new Not<>(left), new Next<>(right)));
+													} else {
+														if (phi instanceof StrongReleaseNode) {
+															// StrongReleaseNode
+															// cphi =
+															// (StrongReleaseNode)
+															// phi;
+															throw new NotImplementedException();
+														} else {
+															if (phi instanceof TrueNode) {
+																return new True<BDD, BDD>();
+															} else {
+																if (phi instanceof UntilNode) {
+																	UntilNode cphi = (UntilNode) phi;
+																	LTLFormula<BDD, BDD> left = getLTLBDD(
+																			cphi.getMyLTL1(), atomToInt, bdds);
+																	LTLFormula<BDD, BDD> right = getLTLBDD(
+																			cphi.getMyLTL2(), atomToInt, bdds);
+
+																	return new Until<BDD, BDD>(left, right);
+																} else {
+																	if (phi instanceof WeakUntilNode) {
+																		WeakUntilNode cphi = (WeakUntilNode) phi;
+																		LTLFormula<BDD, BDD> left = getLTLBDD(
+																				cphi.getMyLTL1(), atomToInt, bdds);
+																		LTLFormula<BDD, BDD> right = getLTLBDD(
+																				cphi.getMyLTL2(), atomToInt, bdds);
+
+																		return new WeakUntil<BDD, BDD>(left, right);
+																	} else {
+																		if (phi instanceof XorNode) {
+																			XorNode cphi = (XorNode) phi;
+
+																			LTLFormula<BDD, BDD> left = getLTLBDD(
+																					cphi.getMyLTL1(), atomToInt, bdds);
+																			LTLFormula<BDD, BDD> right = getLTLBDD(
+																					cphi.getMyLTL2(), atomToInt, bdds);
+
+																			return new Or<BDD, BDD>(new And<BDD, BDD>(
+																					new Not<BDD, BDD>(left), right),
+																					new And<BDD, BDD>(right,
+																							new Not<BDD, BDD>(left)));
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		throw new IllegalArgumentException("Unsupported formula");
+	}
+
 }
