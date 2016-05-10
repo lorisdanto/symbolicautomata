d4e4356fca85bb99c2030e17e046adcb2e8cf2ae
lorisdanto <lorisdanto@gmail.com>
2016-05-10 13:34:52 -0500

    fixed bug in equality solver

--- SVPAlib/src/theory/characters/ICharPred.java
+++ SVPAlib/src/theory/characters/ICharPred.java
@@ -1,5 +1,14 @@
 package theory.characters;
 
-public interface ICharPred {
-
+public abstract class ICharPred {
+	
+	boolean isReturn = false;
+	
+	public void setAsReturn(){
+		isReturn = true;
+	}
+	
+	public boolean isReturn(){
+		return isReturn;
+	}
 }
