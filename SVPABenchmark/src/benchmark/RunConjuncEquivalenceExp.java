60d2ad14cb11b98c87d3e05366d00a92a3089d18
Wang Fang <terrywang2720@gmail.com>
2016-07-01 23:04:25 -0500

    delete old equiv, apply changes to the new one

--- SVPABenchmark/src/benchmark/RunConjuncEquivalenceExp.java
+++ SVPABenchmark/src/benchmark/RunConjuncEquivalenceExp.java
@@ -395,20 +395,24 @@ public class RunConjuncEquivalenceExp {
 		}
 
 		try {
+			long totalTimeLeft = timeOut;
 			long startDate = System.currentTimeMillis();
-			SFA<CharPred, Character> tempLeftSFA = IntersectedSFA(sfaLHS, timeOut);
+			SFA<CharPred, Character> tempLeftSFA = IntersectedSFA(sfaLHS, totalTimeLeft);
 			long endDate = System.currentTimeMillis();
-			long totalTimeLeft = endDate - startDate;
+			totalTimeLeft -= endDate - startDate;
 			startDate = System.currentTimeMillis();
-			SFA<CharPred, Character> tempRightSFA = IntersectedSFA(sfaRHS, timeOut - totalTimeLeft);
+			SFA<CharPred, Character> tempRightSFA = IntersectedSFA(sfaRHS, totalTimeLeft);
 			endDate = System.currentTimeMillis();
-			long totalTimeRight = endDate - startDate;
-			long startDateEquiv = System.currentTimeMillis();
-			tempLeftSFA.isHopcroftKarpEquivalentTo(tempRightSFA, solver, timeOut);
-			long endDateEquiv = System.currentTimeMillis();
-			totalTimeSFA = endDateEquiv - startDateEquiv + totalTimeLeft + totalTimeRight;
+			totalTimeLeft -= endDate - startDate;
+			startDate = System.currentTimeMillis();
+			tempLeftSFA.isHopcroftKarpEquivalentTo(tempRightSFA, solver, totalTimeLeft);
+			endDate = System.currentTimeMillis();
+			totalTimeLeft -= endDate - startDate;
+			totalTimeSFA = timeOut-totalTimeLeft;
+			
 		} catch (Exception e) {
 			totalTimeSFA = timeOut;
+			
 		}
 
 	}
