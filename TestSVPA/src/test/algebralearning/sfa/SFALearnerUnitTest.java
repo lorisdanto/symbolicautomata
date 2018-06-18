2a7eeb7e88f84302b06ece669e4f5b36891a3c67
GeorgeArgyros <gsa2111@columbia.edu>
2018-06-18 13:10:31 -0400

    simplified test cases

--- TestSVPA/src/test/algebralearning/sfa/SFALearnerUnitTest.java
+++ TestSVPA/src/test/algebralearning/sfa/SFALearnerUnitTest.java
@@ -3,6 +3,10 @@ package test.algebralearning.sfa;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 import org.sat4j.specs.TimeoutException;
 
@@ -11,19 +15,32 @@ import algebralearning.sfa.SFAAlgebraLearner;
 import algebralearning.sfa.SFAEquivalenceOracle;
 import algebralearning.sfa.SFAMembershipOracle;
 import automata.sfa.SFA;
-import benchmark.SFAprovider;
+import automata.sfa.SFAInputMove;
+import automata.sfa.SFAMove;
 import theory.characters.CharPred;
 import theory.intervals.UnaryCharIntervalSolver;
 
-import benchmark.algebralearning.RELearning;
-
 public class SFALearnerUnitTest {
+	static UnaryCharIntervalSolver solver = new UnaryCharIntervalSolver();
 
+	private SFA<CharPred, Character> getTestSFA() throws TimeoutException {
+		List <SFAMove<CharPred, Character>> transitions = new LinkedList<>();
+		HashSet <Integer> finalStates = new HashSet<>();
+		
+		transitions.add(new SFAInputMove<>(0, 1, solver.True()));
+		transitions.add(new SFAInputMove<>(1, 1, new CharPred('a')));
+		transitions.add(new SFAInputMove<>(1, 2, new CharPred('b')));
+		CharPred p = new CharPred('c');
+		p = solver.MkOr(p, new CharPred('d'));
+		transitions.add(new SFAInputMove<>(2, 0, p));
+		finalStates.add(2);
+		
+		return SFA.MkSFA(transitions, 0, finalStates, solver);
+	}
+	
 	@Test
-	public void testSFALearning1() throws TimeoutException {
-		UnaryCharIntervalSolver solver = new UnaryCharIntervalSolver();
-		SFAprovider provider = new SFAprovider("aaa", solver);			
-		SFA<CharPred, Character> model, sfa = provider.getSFA().determinize(solver).minimize(solver);
+	public void testSFALearning1() throws TimeoutException {					
+		SFA<CharPred, Character> model, sfa = getTestSFA().determinize(solver).minimize(solver);
 		SFAMembershipOracle<CharPred, Character>memb = new SFAMembershipOracle<>(sfa, solver);
 		SFAEquivalenceOracle<CharPred, Character>equiv = new SFAEquivalenceOracle<>(sfa, solver);
 		EqualityAlgebraLearnerFactory<CharPred, Character> balf = new EqualityAlgebraLearnerFactory<>(solver);
@@ -33,23 +50,5 @@ public class SFALearnerUnitTest {
 		assertTrue(model.isEquivalentTo(sfa, solver));					
 	}
 	
-	@Test
-	public void testSFALearning2() throws TimeoutException {
-		UnaryCharIntervalSolver solver = new UnaryCharIntervalSolver();
-		SFAprovider provider = new SFAprovider("<scrip(t|l)>s(.*)</script>", solver);			
-		SFA<CharPred, Character> model, sfa = provider.getSFA().determinize(solver).minimize(solver);
-		SFAMembershipOracle<CharPred, Character>memb = new SFAMembershipOracle<>(sfa, solver);
-		SFAEquivalenceOracle<CharPred, Character>equiv = new SFAEquivalenceOracle<>(sfa, solver);
-		EqualityAlgebraLearnerFactory<CharPred, Character> balf = new EqualityAlgebraLearnerFactory<>(solver);
-		SFAAlgebraLearner<CharPred, Character> learner = new SFAAlgebraLearner<>(memb, solver, balf);
-		
-		model = learner.getModelFinal(equiv);
-		assertTrue(model.isEquivalentTo(sfa, solver));					
-	}
-	
-	@Test
-	public void testRELearning() throws TimeoutException {
-		RELearning rel = new RELearning();
-		assertTrue(rel.learnREBenchmark(2) != null);		
-	}
+
 }
