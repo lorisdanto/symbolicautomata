ba7be6c8e2f5e148b6bbe7149edb19138294fed9
Loris D'Antoni <loris@cs.wisc.edu>
2018-12-04 15:04:40 -0600

    BA with subs and outputs

---
+++ SVPAlib/src/theory/BooleanAlgebraSubstAndOutputs.java
@@ -0,0 +1,27 @@
+/**
+ * SVPAlib
+ * theory
+ * Apr 21, 2015
+ * @author Loris D'Antoni
+ */
+package theory;
+
+import java.util.List;
+
+/**
+ * BooleanAlgebraSubst: A Boolean Alegbra with substitution
+ * @param <P> The type of predicates forming the Boolean algebra
+ * @param <F> The type of functions S->S in the Boolean Algebra 
+ * @param <S> The domain of the Boolean algebra
+ */
+public abstract class BooleanAlgebraSubstAndOutputs<P, PL, F, S> extends BooleanAlgebraSubst<P, F, S>{
+
+	//All the BA for lists
+	
+	/**
+	 * get the restricted output based on <code>p</code> and <code>f</code>
+	 * @return \psi(y) = \exists x. \phi(x) \wedge f(x)=y
+	 */
+	public abstract PL getRestrictedOutput(P p, List<F> fs, BooleanAlgebra<PL,List<S>> bal);
+	
+}
