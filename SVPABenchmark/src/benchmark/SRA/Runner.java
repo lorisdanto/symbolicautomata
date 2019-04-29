08ca301d3e8c8b09a0215926dd68923b29fdb3a1
Tiago Ferreira <tiago.ferreira@wwchain.com>
2019-04-29 20:45:30 +0100

    Fix bug with anomaly detection

--- SVPABenchmark/src/benchmark/SRA/Runner.java
+++ SVPABenchmark/src/benchmark/SRA/Runner.java
@@ -43,7 +43,6 @@ public class Runner {
     public static void main(String[] args) {
         while(true) {
             try {
-
                 System.out.println("-------------------------------------------------------------------------------");
                 System.out.println("SRA Experiment Runner");
                 System.out.println("-------------------------------------------------------------------------------");
@@ -91,11 +90,21 @@ public class Runner {
                     if (filePath != null)
                         file = new File(filePath);
 
-                    if (runsParsed != null)
+                    if (runsParsed != null) {
                         numberOfRuns = Integer.parseInt(runsParsed);
+                        if (numberOfRuns < 1) {
+                            System.err.println("Error: The number of runs must be at least 1.");
+                            System.exit(1);
+                        }
+                    }
 
-                    if (timeoutParsed != null)
+                    if (timeoutParsed != null) {
                         timeout = Integer.parseInt(timeoutParsed);
+                        if (timeout < 1) {
+                            System.err.println("Error: The timeout must be at least 1s.");
+                            System.exit(1);
+                        }
+                    }
 
                     if (cmd.hasOption("l")) {
                         System.out.println("Benchmarks available:");
@@ -147,39 +156,50 @@ public class Runner {
                             Future<Object> future = null;
 
                             try {
-                                for (Integer iterator = 0; iterator < numberOfRuns; iterator++) {
-                                    Stopwatch timer = Stopwatch.createStarted();
-                                    ExecutorService executor = Executors.newCachedThreadPool();
-                                    Callable<Object> task = new Callable<Object>() {
-                                        public Object call() throws Exception {
-                                            return method.invoke(sra);
-                                        }
-                                    };
-                                    future = executor.submit(task);
-                                    Object result = future.get(timeout, TimeUnit.SECONDS);
+                                int iterations = numberOfRuns;
+                                boolean needsReRun = true;
+                                while (needsReRun) {
+                                    for (Integer iterator = 0; iterator < iterations; iterator++) {
+                                        Stopwatch timer = Stopwatch.createStarted();
+                                        ExecutorService executor = Executors.newCachedThreadPool();
+                                        Callable<Object> task = new Callable<Object>() {
+                                            public Object call() throws Exception {
+                                                return method.invoke(sra);
+                                            }
+                                        };
+                                        future = executor.submit(task);
+                                        Object result = future.get(timeout, TimeUnit.SECONDS);
 
-                                    timer.stop();
-                                    BigDecimal timerResult = BigDecimal.valueOf(timer.elapsed(TimeUnit.NANOSECONDS)).divide(new BigDecimal("1000000000"), roundingPrecision, RoundingMode.UP);
-                                    System.out.println("[" + (iterator + 1) + "] Done in: " + timerResult + " s.");
-                                    timings.add(timerResult);
-
-                                    if (iterator > 2) {
-                                        BigDecimal cutOff = standardDeviation(timings).multiply(BigDecimal.valueOf(3));
-                                        BigDecimal lower = mean(timings).subtract(cutOff);
-                                        BigDecimal upper = mean(timings).add(cutOff);
-                                        if (timerResult.compareTo(lower) < 0 || timerResult.compareTo(upper) > 0) {
-                                            System.out.println("Cut Off: " + cutOff.toString());
-                                            System.out.println("Upper Limit: " + upper.toString());
-                                            System.out.println("Lower Limit: " + lower.toString());
-                                            System.out.println("[" + (iterator + 1) + "] Marked as anomaly. Repeating.");
-                                            iterator--;
-                                        } else {
-                                            validTimings.add(timerResult);
-                                        }
-                                    } else {
-                                        validTimings.add(timerResult);
+                                        timer.stop();
+                                        BigDecimal timerResult = BigDecimal.valueOf(timer.elapsed(TimeUnit.NANOSECONDS)).divide(new BigDecimal("1000000000"), roundingPrecision, RoundingMode.UP);
+                                        System.out.println("[" + (iterator + 1) + "] Done in: " + timerResult + " s.");
+                                        timings.add(timerResult);
                                     }
 
+                                    ArrayList<BigDecimal> newDataSet = new ArrayList<BigDecimal>();
+                                    newDataSet.addAll(timings);
+                                    newDataSet.addAll(validTimings);
+
+                                    BigDecimal cutOff = standardDeviation(newDataSet).multiply(BigDecimal.valueOf(3));
+                                    BigDecimal lower = mean(newDataSet).subtract(cutOff);
+                                    BigDecimal upper = mean(newDataSet).add(cutOff);
+                                    for (BigDecimal timing : timings) {
+                                        if (timing.compareTo(lower) < 0 || timing.compareTo(upper) > 0) {
+                                            System.out.println("[!] " + timing + " Marked as anomaly. Discarding.");
+                                        } else {
+                                            validTimings.add(timing);
+                                        }
+                                    }
+
+                                    timings.clear();
+//                                    timings.addAll(validTimings);
+
+                                    if (validTimings.size() < numberOfRuns) {
+                                        iterations = numberOfRuns - validTimings.size();
+                                        System.out.println("Detected " + iterations + " anomalies in total. Repeating.");
+                                    } else {
+                                        needsReRun = false;
+                                    }
                                 }
 
                                 System.out.println("[AVERAGE] Done in: " + mean(validTimings) + " s.");
@@ -187,8 +207,8 @@ public class Runner {
 
                                 ArrayList<String> outRecord = new ArrayList<String>();
                                 outRecord.add(method.getName());
-                                for (BigDecimal timming : validTimings)
-                                    outRecord.add(timming.toString());
+                                for (BigDecimal timing : validTimings)
+                                    outRecord.add(timing.toString());
 
                                 csvWriter.writeNext(outRecord.stream().toArray(String[]::new));
                                 try {
@@ -250,12 +270,13 @@ public class Runner {
             x1 = A.divide(x0, SCALE, BigDecimal.ROUND_HALF_UP);
             x1 = x1.add(x0);
             x1 = x1.divide(BigDecimal.valueOf(2), SCALE, BigDecimal.ROUND_HALF_UP);
-
         }
         return x1;
     }
 
     private static BigDecimal standardDeviation(ArrayList<BigDecimal> results) {
+        if (results.size() < 2)
+            return BigDecimal.ZERO;
         BigDecimal mean = mean(results);
         BigDecimal sum = BigDecimal.ZERO;
         for (BigDecimal value : results)
